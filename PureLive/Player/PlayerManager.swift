ead8078f1fc18fd3004c984333d89fdf2da43b9f
Kluoke <1125919032@qq.com>
2026-09-14 11:33:42 +0800

    feat: add AVPlayer manager

---
+++ PureLive/Player/PlayerManager.swift
@@ -0,0 +1,35 @@
+import AVFoundation
+import Foundation
+
+@MainActor
+final class PlayerManager: ObservableObject {
+    @Published private(set) var player: AVPlayer?
+    @Published private(set) var isPlaying = false
+    @Published private(set) var error: Error?
+
+    func load(_ stream: LiveStream, autoplay: Bool = true) {
+        error = nil
+        let item = AVPlayerItem(url: stream.url)
+        player = AVPlayer(playerItem: item)
+        if autoplay {
+            player?.play()
+            isPlaying = true
+        }
+    }
+
+    func play() {
+        player?.play()
+        isPlaying = true
+    }
+
+    func pause() {
+        player?.pause()
+        isPlaying = false
+    }
+
+    func stop() {
+        player?.pause()
+        player = nil
+        isPlaying = false
+    }
+}
