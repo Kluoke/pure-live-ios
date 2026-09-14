854428baca7627732a9188200631767ab262bf7d
Kluoke <1125919032@qq.com>
2026-09-14 11:37:38 +0800

    Register native live platform adapters

--- PureLive/Platforms/PlatformRegistry.swift
+++ PureLive/Platforms/PlatformRegistry.swift
@@ -1,13 +1,17 @@
 import Foundation
 
 struct PlatformRegistry: Sendable {
+    static let live = PlatformRegistry(services: [
+        .bilibili: BilibiliService(),
+        .douyu: DouyuService(),
+        .huya: HuyaService(),
+        .kuaishou: KuaishouService(),
+        .douyin: DouyinService(),
+        .neteaseCC: NetEaseCCService()
+    ])
+
     private let services: [LivePlatform: any LivePlatformService]
 
-    init(services: [LivePlatform: any LivePlatformService] = [:]) {
-        self.services = services
-    }
-
-    func service(for platform: LivePlatform) -> (any LivePlatformService)? {
-        services[platform]
-    }
+    init(services: [LivePlatform: any LivePlatformService] = [:]) { self.services = services }
+    func service(for platform: LivePlatform) -> (any LivePlatformService)? { services[platform] }
 }
