8a0525f4654047f88ddaee89a5ea6186450721c3
Kluoke <1125919032@qq.com>
2026-09-14 11:37:47 +0800

    Remove IPTV/M3U8 platform from native app

--- PureLive/Models/LivePlatform.swift
+++ PureLive/Models/LivePlatform.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// Live-stream platforms supported by the native application.
+/// Native live-stream providers supported by the application.
 enum LivePlatform: String, CaseIterable, Identifiable, Codable, Sendable {
     case all
     case bilibili
@@ -9,20 +9,18 @@ enum LivePlatform: String, CaseIterable, Identifiable, Codable, Sendable {
     case kuaishou
     case douyin
     case neteaseCC
-    case iptv
 
     var id: String { rawValue }
 
     var title: String {
         switch self {
-        case .all: return "All"
-        case .bilibili: return "Bilibili"
-        case .douyu: return "Douyu"
-        case .huya: return "Huya"
-        case .kuaishou: return "Kuaishou"
-        case .douyin: return "Douyin"
-        case .neteaseCC: return "NetEase CC"
-        case .iptv: return "M3U8 / IPTV"
+        case .all: return "全部"
+        case .bilibili: return "哔哩哔哩"
+        case .douyu: return "斗鱼"
+        case .huya: return "虎牙"
+        case .kuaishou: return "快手"
+        case .douyin: return "抖音"
+        case .neteaseCC: return "网易CC"
         }
     }
 
@@ -31,7 +29,6 @@ enum LivePlatform: String, CaseIterable, Identifiable, Codable, Sendable {
         case .all: return "square.grid.2x2"
         case .bilibili: return "play.tv"
         case .douyu, .huya, .kuaishou, .douyin, .neteaseCC: return "dot.radiowaves.left.and.right"
-        case .iptv: return "antenna.radiowaves.left.and.right"
         }
     }
 }
